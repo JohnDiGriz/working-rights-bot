a7cde011ba96ef57147141562118c36511a8ea14
Serhii Havrylov <shavrylov@tennis-point.de>
2022-03-08 15:16:35 +0200

    Use chat id

--- bot.py
+++ bot.py
@@ -11,60 +11,46 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = os.environ["TOKEN"]
 
+
 def start(update, context):
     update.message.reply_text("""Відправте свою скаргу
 Отправьте свою жалобу""")
 
 
 def help(update, context):
-    """Send a message when the command /help is issued."""
     update.message.reply_text("""Використайте команду /compaint щоб відправити скаргу
 Используйте комманду /complaint чтоб отправить жалобу""")
 
 
-def receiveComplaint(update, context):
+def receive_complaint(update, context):
     update.message.reply_text("""Ваша скарга прийнята
 Ваша жалоба принята""")
-    update.message.forward(chat_id='@workingrightsbotchannel')
-    context.bot.send_message(chat_id='@workingrightsbotchannel', text=update.message.from_user.username)
-    logging.warning("Forwarded from %s", update.message.forward_from_chat)
+    update.message.forward(chat_id=-1001773404147)
+    context.bot.send_message(chat_id=-1001773404147, text="@"+update.message.from_user.username)
 
 
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
 
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("complaint", start))
     dp.add_handler(CommandHandler("help", help))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, receiveComplaint))
+    dp.add_handler(MessageHandler(Filters.text, receive_complaint))
 
-    # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN,
                           webhook_url='https://working-rights-bot.herokuapp.com/' + TOKEN)
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
